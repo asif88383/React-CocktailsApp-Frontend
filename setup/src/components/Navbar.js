07c41e720b09169df3b344658452d2ab266a9400
Muhammad Asif <asif88383@gmail.com>
2022-04-09 12:53:42 +0500

    added navbar

--- setup/src/components/Navbar.js
+++ setup/src/components/Navbar.js
@@ -5,7 +5,21 @@ import About from '../pages/About'
 
 const Navbar = () => {
   return (
-    
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='cocktail db logo' className='logo' />
+        </Link>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='about'>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
