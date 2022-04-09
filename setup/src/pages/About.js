520d6d33c0063a69b12cf2116f3a18c9cf8e3cdc
Muhammad Asif <asif88383@gmail.com>
2022-04-09 12:57:01 +0500

    added about us

--- setup/src/pages/About.js
+++ setup/src/pages/About.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Mauris eget nisl id libero consectetur euismod.
+        Nulla euismod, nunc eget euismod consectetur,
+        nisi nisl aliquam nunc, euismod aliquam nunc nisi eget nisl.
+        Nulla euismod, nunc eget euismod consectetur,
+        nisi nisl aliquam nunc, euismod aliquam nunc nisi eget nisl.Nulla euismod, 
+        nunc eget euismod consectetur, nisi nisl aliquam nunc, 
+        euismod aliquam nunc nisi eget nisl.Nulla euismod, 
+        unc eget euismod consectetur, nisi nisl aliquam nunc, 
+        euismod aliquam nunc nisi eget nisl.Nulla euismod, 
+        nunc eget euismod consectetur, nisi nisl aliquam nunc, 
+        euismod aliquam nunc nisi eget nisl.
+      </p>
+    </section>
   )
 }
 
