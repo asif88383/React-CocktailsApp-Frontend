15b92e277f33ee642db8f814b48b9faf821afe1d
Muhammad Asif <asif88383@gmail.com>
2022-04-10 12:12:02 +0500

    updated singleCocktail

--- setup/src/pages/SingleCocktail.js
+++ setup/src/pages/SingleCocktail.js
@@ -4,6 +4,67 @@ import { useParams, Link } from 'react-router-dom'
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
+export default function SingleCocktail(){
+  const {id} = useParams()
+  const [loading, setLoading] = React.useState(false)
+  const [cocktail, setCocktail] = React.useState(null)
+
+  React.useEffect(() => {
+    setLoading(true)
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`)
+        const data = await response.json()
+        if(data.drinks){
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0]
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ]
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          }
+          setCocktail(newCocktail)
+        }else{
+          setCocktail(null)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      setLoading(false)
+    }
+    getCocktail()
+  }, [id])
+  if(loading){
+    return <Loading/>
+  }
+  if(!cocktail){
+    return <h2 className='section-title'>Cocktail not found</h2>
+  }
+}
+
+
 const SingleCocktail = () => {
   return (
     <div>
@@ -12,4 +73,3 @@ const SingleCocktail = () => {
   )
 }
 
-export default SingleCocktail
