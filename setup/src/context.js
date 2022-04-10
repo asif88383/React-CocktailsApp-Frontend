8e9e9e0548d7ca1a6dc6d54196d90b9cc0588cf6
Muhammad Asif <asif88383@gmail.com>
2022-04-10 11:24:39 +0500

    updated context

--- setup/src/context.js
+++ setup/src/context.js
@@ -8,6 +8,29 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(false)
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
+
+  const fetchDrinks = async () => {
+    setLoading(true)
+    try{
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json()
+      const {drinks} = data
+      
+      if(drinks){
+      }else{
+        setCocktails([])
+      }
+    }
+    catch(error){
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
+
   return (
     <AppContext.Provider
      value={{
